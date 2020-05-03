de771bc438a1e4bf6fc620feb8a42151abda42da
koed <koed@L097105.corp.ads>
2020-05-02 21:45:15 -0400

    ver1

--- src/main/java/com/in28minutes/springboot/tutorial/basics/application/configuration/WelcomeController.java
+++ src/main/java/com/in28minutes/springboot/tutorial/basics/application/configuration/WelcomeController.java
@@ -1,4 +1,4 @@
-package com.example.herokudemo;
+package com.in28minutes.springboot.tutorial.basics.application.configuration;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 //http://localhost:8080/index
 @CrossOrigin(origins = "*", allowedHeaders = "*") // "http://localhost:8080")
 @Controller
-public class IndexController {
+public class WelcomeController {
 
     @RequestMapping("/welcome")
     public String loginMessage() {
